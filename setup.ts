c261ffa1936008986c1f430a12d965576a1f43b2
Greg <greg@ruboss.com>
2021-04-02 16:26:16 -0700

    setup fille

---
+++ setup.ts
@@ -0,0 +1,13 @@
+import { RunResult } from "sqlite3";
+import { Database } from "./lib/base_relation";
+import { sql } from "./lib/sql";
+
+Database.run(sql`
+  CREATE TABLE IF NOT EXISTS test(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255)
+  );
+`).then((res: RunResult) => {
+  console.log("yay");
+  process.exit(0);
+});
